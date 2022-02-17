547d57b4b08c30ac8b71efecd93a1066062ee42c
don <misurakalu@gmail.com>
2022-02-17 15:23:13 +1000

    Initial Commit - Add swift files
     (Linkedin Learning and Apple Swift UI Documentation)

---
+++ CreatingAndCombiningViews/Working/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Don Misura Kaluarachchi on 17/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LL Exercise Files/AdventureSwift_Working/Ch2_Variable_Basics.playground/Pages/Working with strings.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+/*:
+ # Working with Strings
+ ---
+ 
+ ## Topic Essentials
+ There are several `String` methods that you'll be using on a regular basis, which include `contains`, `append`, `insert`, `remove`, and `split`. Refer to the documentation to see everything the `String` class offers.
+ 
+ ### Objectives
+ + Retrieve state information about a string using `count` and `isEmpty`
+ + Use each of the mentioned class methods to alter the starting string
+ 
+  [Previous Topic](@previous)                                                 [Next Topic](@next)
+ 
+ */
+// Test variable
+var dialogue = "the Innkeeper's Haven"
+
+// String data
+//Access the properties of the methods or class :)
+dialogue
+
+// Append and Insert
+
+
+// Remove and Split
+
